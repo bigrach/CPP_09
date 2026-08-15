51c414955dbb8b934b15f4267b46aa7b7cee4146
rach 42 <rlebigre@student.42angouleme.fr>
2026-08-15 19:15:55 +0200

    cleaned up

--- ex00/inc/BitcoinExchange.hpp
+++ ex00/inc/BitcoinExchange.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   btc.hpp                                            :+:      :+:    :+:   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlebigre <rlebigre.42angouleme@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 18:24:28 by rlebigre          #+#    #+#             */
-/*   Updated: 2026/08/14 18:40:13 by rlebigre         ###   ########.fr       */
+/*   Updated: 2026/08/15 19:12:53 by rlebigre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,49 @@
  #define EASYFIND_HPP
 
 #include <string>
+#include <map>
 #include <iostream>
-#include <algorithm>
+#include <fstream>
+
+// EASIER NAME FOR THE MAP 
+typedef std::map<std::string,double> thisMap;
 
 // COLORS
  #define RESET "\x1b[0m"
  #define RED "\x1B[1;91m"
- #define PURPLE "\e[1;35m"
- #define DBLUE "\e[1;36m"
+ #define DBLUE "\x1b[1;36m"
+ #define PURPLE "\x1B[1;35m"
+// MONTHS
+enum e_months {
+	JANUARY = 1,
+	FEBRUARY,
+	MARCH,
+	APRIL,
+	MAY,
+	JUNE,
+	JULY,
+	AUGUST,
+	SEPTEMBER,
+	OCTOBER,
+	NOVEMBER,
+	DECEMBER,
+};
 
- #define DataFormat ", "
- #define InputFormat " |"
-// bitcoinexchange name
+// INPUT LINE BENCHMARKS
+enum e_where {
+	END_DATE = 10,
+	START_VALUE = 11,
+	MIN_LEN = 12,
+};
+
+// FORMAT
+ #define DataFormat ","
+ #define InputFormat " | "
+ #define InputFormatLen 3
+
+// FUNCTIONS
+int								check_files(int argc, char **argv, std::ifstream &database, std::ifstream &input);
+std::map<std::string,double>	make_map(std::ifstream &file);
+void							study_input(thisMap data, std::ifstream &file);
+
 #endif
---
+++ ex00/src/BitcoinExchange.cpp
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlebigre <rlebigre.42angouleme@gmail.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/15 18:53:11 by rlebigre          #+#    #+#             */
+/*   Updated: 2026/08/15 19:13:03 by rlebigre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+#include <vector>
+#include <cstring>
+#include <limits.h>
+#include <cstdlib>
+
+bool	is_leapyear(int year)
+{
+	return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+
+int	check_date_validity(std::string input)
+{
+	const size_t BASE = 10;
+
+	int year = std::strtol(input.c_str(), NULL, BASE);
+	int month = std::strtol(&input[5], NULL, BASE);
+	int day = std::strtol(&input[8], NULL, BASE);
+
+	switch (month)
+	{
+		case APRIL: case JUNE: case SEPTEMBER: case NOVEMBER:
+			return (day > 30);
+
+		case JANUARY: case MARCH: case MAY: case JULY: case AUGUST: case OCTOBER: case DECEMBER:
+			return (day > 31);
+
+		case FEBRUARY:
+			if (is_leapyear(year))
+				return (day > 29);
+			return (day > 28);
+
+		default:
+			std::cout << "salut " << std::endl;
+			return 1;
+	}
+}
+
+int	decent_date(std::string input)
+{
+	std::string dateFormat = "xxxx-xx-xx";
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (dateFormat[i] == 'x' && !isdigit(input[i]))
+			return 1;
+		if (dateFormat[i] == '-' && input[i] != '-')
+			return 1;
+	}
+
+	if (check_date_validity(input))
+		return 1;
+
+	return 0;
+}
+
+std::map<std::string,double>	make_map(std::ifstream &file)
+{
+	std::string line;
+	thisMap currentMap;
+	
+	std::getline(file, line);
+	if (line != "date,exchange_rate")
+		throw "Missing 'date,exchange_rate' first line";
+	while (!file.eof())
+	{
+		std::getline(file, line);
+		if (line.length() < MIN_LEN)
+			throw "Line not long enough to have all data.";
+		if (decent_date(line))
+		{
+			std::cout << "HERE--------- "<< line << std::endl;
+			throw "Invalid date.";
+		}
+		if (line[10] != *DataFormat)
+			throw "Invalid format.";
+		char *leftovers = NULL;
+		double value = strtold(&line[START_VALUE], &leftovers);
+		if (*leftovers != '\0')
+			throw "Shit at the end of the line.";
+
+		currentMap[line.substr(0, END_DATE)] = value; 
+	}
+	return currentMap;
+}
+
+int	make_calculations(thisMap data, double howmany, std::string &whatday)
+{
+	thisMap::iterator found = data.lower_bound(whatday);
+	if (found == data.begin() && found->first != whatday)
+		return 1;
+	if (found == data.end() || (found->first != whatday))
+		--found;
+	std::cout << DBLUE << whatday << " => " << howmany << " = " << found->second * howmany << RESET << std::endl;
+	return 0;
+}
+
+void	study_input(thisMap data, std::ifstream &file)
+{
+	std::string line;
+	char *leftovers = NULL;
+
+	std::getline(file, line);
+	if (line != "date | value")
+		throw "Missing 'date | value' first line in input file.";
+	while (!file.eof())
+	{
+		std::getline(file, line);
+		if (line.length() < 14)
+			std::cout << RED "Error: bad input => " PURPLE "'" << line << "'" RESET << std::endl;
+		else if (decent_date(line))
+			std::cout << RED "Error: date is invalid => " PURPLE "'" << line.substr(0, 10) << "'" RESET << std::endl;
+		else if (strncmp(&line[10], InputFormat, InputFormatLen) != 0)
+			std::cout << RED "Error: bad format (date and value not separated by ' | ') => " PURPLE "'" << line << "'" RESET << std::endl;
+		else {
+			std::string whatday = line.substr(0, 10);
+			double value = strtold(&line[12], &leftovers);
+			if (value < 0)
+				std::cout << RED "Error: not a positive number => " PURPLE "'" << value << "'" RESET << std::endl;
+			else if (value > (double)1000)
+				std::cout << RED "Error: too large a number => " PURPLE "'" <<  value << "'" RESET << std::endl;
+			else if (*leftovers != '\0')
+				std::cout << RED "Error: shit in line => " PURPLE "'" << leftovers << "'" RESET << std::endl;
+			else if (make_calculations(data, value, whatday))
+				std::cout << RED "Error: date is too early => " PURPLE "'" << whatday << "'" RESET << std::endl;
+		}
+	}
+}
